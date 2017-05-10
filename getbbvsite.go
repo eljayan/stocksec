75fca91b5cb6930c62e947dba586ce259334d2f0
Mishell Hurtado Maldonado <MisHurtado@Mishells-MacBook-Air.local>
2017-05-09 21:31:38 -0500

    First commit for real this time

---
+++ getbbvsite.go
@@ -0,0 +1,8 @@
+package main
+
+import "net/http"
+
+func getsite(){
+	println("getting site!")
+
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "getbvgsite"
+
+func main(){
+    //get the website
+	getsite()
+
+    //parse it
+
+    //save it on a database
+
+    //analize the prices
+
+    //send me an email notification
+}
